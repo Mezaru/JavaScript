5c329ac5c3a09c649e9678553faa65d1ce4ccc7c
Marcus D <Marcus@academy.se>
2018-03-13 14:29:02 +0100

    bla bla bla

---
+++ code.js
@@ -0,0 +1,47 @@
+function renderMovie(movie) {
+    $("#image").attr("src", movie.image);
+    // let image = document.getElementById("image");
+    // image.setAttribute("src", movie.image);
+
+    $("#title").text(movie.title);
+    // let title = document.getElementById("title");
+    // title.innerText = movie.title;
+
+    $("#description").text(movie.description);
+    // let description = document.getElementById("description");
+    // description.innerText = movie.description;
+
+
+    $("#actors").html(function (actor){
+        actor = "";
+        for (let index = 0; index < movie.actors.length; index++) {
+        actor += "<li>" + movie.actors[index] + "</li>";
+    }
+    return actor;
+    });
+    // let actors = document.getElementById("actors");
+    // let actor = "";
+    // for (let index = 0; index < movie.actors.length; index++) {
+    //     actor += "<li>" + movie.actors[index] + "</li>";
+    // }
+    // actors.innerHTML = actor;
+}
+
+function changeStarRating(rating) { 
+    for (let i = 1; i <= 5; i++) {
+        let star = $("#star" + i);
+        if (i <= rating){
+            star.addClass("filled");
+          } else {
+            star.removeClass("filled");
+          }
+    }
+}
+
+
+for (let index = 1; index <= 5; index++) {
+    $("#star" + index).click(function(){
+        changeStarRating(index);
+    });
+    
+}
---
+++ data.js
@@ -0,0 +1,6 @@
+let movieData = {
+    title: "Avengers",
+    description: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+    actors: ["Robert Downey Jr.", "Chris Evans", "Scarlett Johansson"],
+    image: "https://www.platekompaniet.no/globalassets/filmcover/2016/august/avengers.jpg"
+}
